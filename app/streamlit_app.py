0f0e5a638dfe7a041d45755439c163467dea0ceb
vmelero13 <v_melero@hotmail.com>
2026-06-01 16:20:09 +0200

    Web APP Update

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# --------------------------------------------------
-# CONFIGURACIÓN
+# -------------------------------------------------- CONFIGURACIÓN -----------------------
 
 
 st.set_page_config(
